2458b2cbeea7e2b1e0c7d8dc8c3d9b7e8e8be748
ZWebD <zoltan.szabo.web@gmail.com>
2023-11-01 11:19:26 +0000

    test

--- components/intro.tsx
+++ components/intro.tsx
@@ -69,7 +69,8 @@ export default function Intro() {
       >
         <Link
           href="#contact"
-          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition
+          dark:bg-black dark:bg-opacity-80"
           onClick={() => {
             setActiveSection("Contact");
             setTimeOfLastClick(Date.now());
