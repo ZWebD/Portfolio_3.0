535ff066b410539a950c01f9836375c0a394f777
ZWebD <zoltan.szabo.web@gmail.com>
2024-06-13 19:49:13 +0100

    Changed email address

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -25,7 +25,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "zoltandev@gmail.com",
+      to: "zoltanszdev@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail as string,
       react: React.createElement(ContactFormEmail, {
